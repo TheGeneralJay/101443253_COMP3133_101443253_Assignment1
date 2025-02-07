1027da7a524c5b67e36d48d55a33d35be6e6554d
Amanda <amandagurns@hotmail.ca>
2025-02-07 07:54:29 -0500

    Minor change

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const mongoose = require("mongoose");
 const DB = require("./database/db");
-const port = 3000;
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
