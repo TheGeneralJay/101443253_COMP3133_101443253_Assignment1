c4591560330d951bda221f3b170ff1dff75ebf36
Amanda <amandagurns@hotmail.ca>
2025-03-31 16:24:09 -0400

    minor change

--- src/app.js
+++ src/app.js
@@ -22,11 +22,18 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
-// const corsOrigin = {
-//   origin: "http://localhost:4200",
-//   credentials: true,
-//   optionSuccessStatus: 200,
-// };
+const corsOrigin = {
+  origin: "http://localhost:4200",
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Request-Headers",
+  ],
+  credentials: true,
+  enablePreflight: true,
+  optionSuccessStatus: 200,
+};
 
 app.use((req, res, next) => {
   const origin = "http://localhost:4200";
@@ -41,8 +48,9 @@ app.use((req, res, next) => {
 
 app.listen(PORT, async () => {
   await server.start();
+  app.use(cors(corsOptions));
+  app.options("*", cors(corsOptions));
   app.use("/graphql", expressMiddleware(server));
-  app.use(cors());
 
   console.log(`-------------------------------------------`);
   console.log(`App listening at http://localhost:${PORT}`);
